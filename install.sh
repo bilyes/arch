5cc373ad53bdc0c7f1a32ce548c22ec9868c3fb8
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2020-11-28 20:08:00 -0500

    Remove parallelism.

--- install.sh
+++ install.sh
@@ -50,7 +50,8 @@ sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /et
 
 # Move AUR instlalation script to the user's home folder
 mv 3-software-aur.sh /home/$username/
-./1-base.sh & runuser -l $username -c './3-software-aur.sh /home/$username'
+./1-base.sh
+runuser -l $username -c './3-software-aur.sh /home/$username'
 
 ./9-configuration.sh
 
