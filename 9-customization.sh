c05d1b20ced7579c65644991a5cfae247aed5486
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2020-11-29 13:54:04 -0500

    Fix a sed command in the customization script.

--- 9-customization.sh
+++ 9-customization.sh
@@ -7,7 +7,7 @@ git clone https://github.com/ilyessbachiri/material-awesome ~/.config/awesome &
 pip install powerline-status &
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k/powerlevel10k"/g' ~/.zshrc &
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc &
 
 # Dot files
 git clone https://github.com/ilyessbachiri/dotfiles.git ~/.config/dotfiles \
