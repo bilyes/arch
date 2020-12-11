6346f7d1589b30d0812559e16e1351b258335361
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2020-12-11 07:47:53 -0500

    Install fzf.

--- 9-customization.sh
+++ 9-customization.sh
@@ -7,7 +7,9 @@ git clone https://github.com/ilyessbachiri/material-awesome ~/.config/awesome &
 (pip install powerline-status && pip install powerline-mem-segment) &
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc &
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+echo "source /usr/share/fzf/key-bindings.zsh" >> ~/.zshrc
+echo "source /usr/share/fzf/completion.zsh" >> ~/.zshrc
 
 # Dot files
 git clone https://github.com/ilyessbachiri/dotfiles.git ~/.config/dotfiles \
