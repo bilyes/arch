04dc451193b1d72735c30f9922b8c7e2e042c068
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2020-12-19 21:09:44 -0500

    Move fzf sourcing out of the customization script.

--- 9-customization.sh
+++ 9-customization.sh
@@ -8,8 +8,6 @@ git clone https://github.com/ilyessbachiri/material-awesome ~/.config/awesome &
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
-echo "source /usr/share/fzf/key-bindings.zsh" >> ~/.zshrc
-echo "source /usr/share/fzf/completion.zsh" >> ~/.zshrc
 
 # Dot files
 git clone https://github.com/ilyessbachiri/dotfiles.git ~/.config/dotfiles \
